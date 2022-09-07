491600fe50eb45ac2d0eae9e3c6f5862e3ad19fa
Deepak Sirohiwal <deepaksirohiwall@gamil.com>
2022-09-07 14:48:38 +0530

    functional stremlit webpage

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
 import streamlit as st
 import pickle
 
-movie_list= pickle.load(open('movie_name.pkl','rb'))
-movie_list=movie_list['title_x'].values
+
+cosine_sim=pickle.load(open('cosine.pkl','rb'))
+#recommendation function 
+def get_recommendations(title,cosine_sim=cosine_sim):
+
+    #index of the movie
+    idx=indices[title]
+    #pairwise similarity scores of all movies with the movie
+    sim_scores=list(enumerate(cosine_sim[idx]))
+
+    #sort the sim_scores and taking the top 10 movies
+    sim_scores=sorted(sim_scores,key=lambda x:x[1],reverse=True) #sorted on the basis of the element on index 1
+    
+    sim_scores=sim_scores[1:11]
+
+    movie_indices=[i[0] for i in sim_scores]    
+
+    #return top 10 movies
+    return movie_list_df['title_x'].iloc[movie_indices]
+    
+#fecting movies poster and discription
+'''
+a. fetch the poster and display
+b. fetch the movie discription
+addition details 
+In which platform is it available
+'''
+
+
+
+#importing movie details
+movie_list_df= pickle.load(open('movie_name.pkl','rb'))
+movie_list=movie_list_df['title_x'].values
+#index of the movie
+indices=pickle.load(open('indices.pkl','rb'))
+
+#streamlit UI
 st.title('Movie Recommender System')
 
 
 selected_movie = st.selectbox(
      'Type movie/select movie name',
      movie_list)
+
+
 if st.button('Recommend me !'):
-     st.write(selected_movie)
+     st.write(get_recommendations(selected_movie))
+
 
